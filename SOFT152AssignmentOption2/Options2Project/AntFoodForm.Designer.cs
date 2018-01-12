3b657939fd7a1bcb7b847874588ae039be5a4b57
adenwebb21 <33032049+adenwebb21@users.noreply.github.com>
2018-01-12 15:58:39 +0000

    Added a list of controls available to the player

--- SOFT152AssignmentOption2/Options2Project/AntFoodForm.Designer.cs
+++ SOFT152AssignmentOption2/Options2Project/AntFoodForm.Designer.cs
@@ -33,20 +33,25 @@
             this.startButton = new System.Windows.Forms.Button();
             this.stopButton = new System.Windows.Forms.Button();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // drawingPanel
             // 
             this.drawingPanel.BackColor = System.Drawing.Color.White;
-            this.drawingPanel.Location = new System.Drawing.Point(42, 93);
+            this.drawingPanel.Location = new System.Drawing.Point(42, 107);
             this.drawingPanel.Name = "drawingPanel";
-            this.drawingPanel.Size = new System.Drawing.Size(373, 320);
+            this.drawingPanel.Size = new System.Drawing.Size(554, 349);
             this.drawingPanel.TabIndex = 0;
             this.drawingPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.drawingPanel_MouseClick);
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(57, 30);
+            this.startButton.Location = new System.Drawing.Point(57, 19);
             this.startButton.Name = "startButton";
             this.startButton.Size = new System.Drawing.Size(75, 23);
             this.startButton.TabIndex = 1;
@@ -56,7 +61,7 @@
             // 
             // stopButton
             // 
-            this.stopButton.Location = new System.Drawing.Point(260, 30);
+            this.stopButton.Location = new System.Drawing.Point(57, 69);
             this.stopButton.Name = "stopButton";
             this.stopButton.Size = new System.Drawing.Size(75, 23);
             this.stopButton.TabIndex = 2;
@@ -69,16 +74,58 @@
             this.timer.Interval = 20;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(184, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(340, 88);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Left mouse click - Add a new food source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Right mouse click - Add a new nest for the worker ants";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(289, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Middle mouse click - Add a nest of aggressive \"robber\" ants";
+            // 
             // AntFoodForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(457, 454);
+            this.ClientSize = new System.Drawing.Size(641, 490);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.stopButton);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.drawingPanel);
             this.Name = "AntFoodForm";
             this.Text = "Steering";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -89,6 +136,10 @@
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Button stopButton;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
